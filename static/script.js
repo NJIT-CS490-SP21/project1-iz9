b39008d16aba3cde8b1c151299d3421aee1e4b98
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2021-02-15 03:52:19 +0000

    Added the function to display results in html

--- static/script.js
+++ static/script.js
@@ -43,6 +43,44 @@
         });
     });
 
-    
+    function showResults(response){
+
+        response = response.artists || response.albums;
+        var myHtml = ''; //to add result 
+        if (response.items.length > 0) {
+            for (var i = 0; i < response.items.length; i++) {
+                // console.log(response.items[i].name);
+                var spotifyUrl = response.items[i].external_urls.spotify;
+                var myName = '<a target="_blank" rel="noopener noreferrer" href="'+spotifyUrl+'"><div class="annoying">' + response.items[i].name + '</div></a>';
+                var imageUrl = 'static/no-image.jpg';
+                
+                if (response.items[i].images[0]) {
+                    imageUrl = response.items[i].images[0].url;
+                }
+                var myImage = '<a target="_blank" rel="noopener noreferrer" href="'+spotifyUrl+'"><img src="'+imageUrl+'" alt="result image"></a>';
+                myHtml += '<div class = "result-box">' + myImage + myName + '</div>';
+            }
+            if (clickedMore) {
+                $('#results-container').append(myHtml);
+            } else {
+                var searchTerm = '<h2>Results for "'+userInput+'"</h2>';
+                $('#search-term').html(searchTerm);
+                $('#results-container').html(myHtml);
+            }
+            // get next set of 20 results ("more" button):
+            // only use replace if there is a url to replace on:
+            nextUrl = response.next && response.next.replace('https://api.spotify.com/v1/search', 'https://elegant-croissant.glitch.me/spotify');
+            // console.log('corrected nextUrl: ', nextUrl);
+            if (nextUrl) {
+                $('#more').addClass('show');
+            } else {
+                $('#more').removeClass('show');
+            }
+        } else {
+            var nonexistent = '<h2>Sorry, there are no results for "'+ userInput +'"</h2>';
+            $('#search-term').html(nonexistent);
+        }
+
+    }
 
 }());
