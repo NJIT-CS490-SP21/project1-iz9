c3428a17af1ac7207ebbda41920ab8f1f1ad9fdf
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2021-02-15 05:54:39 +0000

    Minor change, randomized artists

--- spotify.py
+++ spotify.py
@@ -41,15 +41,17 @@ headers = {
 
 ##############################################################################
 
-# randomly choose an artist ID from a list
-# Justin Bieber, Beyonce, Chris Brown 
-id_list = ['1uNFoZAHBGtllmzznpCI3s','7bXgB6jMjp9ATFy66eO08Z', '6vWDO969PvNqNYHIOW5v0m']
-artistID = random.choice(id_list)
 
-BASE_URL ='https://api.spotify.com/v1/artists/{}/top-tracks'.format(artistID)
 
 def get_info():
     '''Returns a list of a chosen song info: artist, name, url, image'''
+    
+    # randomly choose an artist ID from a list
+    # Justin Bieber, Beyonce, Chris Brown 
+    id_list = ['1uNFoZAHBGtllmzznpCI3s','7bXgB6jMjp9ATFy66eO08Z', '6vWDO969PvNqNYHIOW5v0m']
+    artistID = random.choice(id_list)
+
+    BASE_URL ='https://api.spotify.com/v1/artists/{}/top-tracks'.format(artistID)
 
     params = {'market' : 'US'}
         
